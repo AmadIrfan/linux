6e7f76e26bab84115f163861cf275016c80a4996
Amad <amadirfan443@gmail.com>
2023-09-23 06:47:56 -0700

    bash update

---
+++ Bash/one.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# printing bash scripts
+# echo "today is "`date`
+# echo "today is $(date)"
+
+# echo `which bash`
+# echo "$(which bash)"
+
+# echo "$(touch two.sh)"
+# echo "$(ls -a)"
+
+# taking input and print on screen
+# echo "Enter your name : "
+# read the_name
+# echo $the_name
+
+# a="Amad irfan"
+# b=$a
+# echo $a
+# echo $b
+
+# taking input from terminal
+# echo "Hello $0 $1"
+
+echo "Enter your name : "
+read myName
+echo "Your Name is $myName"
+
+if [[ $the_name=="amad" ]];
+then
+    echo "amadIRfan"
+fi
